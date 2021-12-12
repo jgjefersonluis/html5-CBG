180fe6ab043a07d8e05c68ef2233c1f352e8f8bc
Jeferson Luis <jgjefersonluis@gmail.com>
2021-12-12 02:09:19 -0300

    formularios simples

---
+++ 3-formularios/1-formulario/pagina2.php
@@ -0,0 +1,5 @@
+<?php
+   echo "el monbre de usuario es: ".$_POST["nombre"];
+   echo "<br>";
+   echo "El apellido es: ".$_POST["apellido"];
+?>
---
+++ 3-formularios/1-formulario/pagina3.php
@@ -0,0 +1,5 @@
+<?php
+   echo "el monbre de usuario es: ".$_GET["nombre"];
+   echo "<br>";
+   echo "El apellido es: ".$_GET["apellido"];
+?>
